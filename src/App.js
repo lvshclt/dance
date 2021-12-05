611081c2ce4483d33e32c18831b729276eb602a1
lvshclt <lvshclt@gmail.com>
2021-12-05 09:35:06 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App container col-lg-12">
+      <div className="row">
+        <div className="col-lg-6"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
